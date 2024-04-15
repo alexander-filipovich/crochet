b82711cd616c422e8ccb0d6f4dc196dedca8ead2
Iana Filipovich <ifilipovich@temporal-games.com>
2024-04-15 21:28:17 +0200

    Menu moved to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GridComponent],
+  imports: [RouterOutlet, GridComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
